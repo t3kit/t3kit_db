f4847a62c81def2a903e23a4d2b94934b2bf1999
Lauri Matti Saarni <lauri@web-essentials.asia>
2016-09-28 16:35:04 +0700

    [FEATURE] Docker DB version
    A Docker fork of database commands to be used with dockert3kit wrapper.
    Can be runned inside db container.

--- restoredb.sh
+++ restoredb.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-PASSWORD="t3kit1234"
-DATABASE="t3kit"
-USERNAME="t3kit"
+PASSWORD="root"
+DATABASE="dockertypo3"
+USERNAME="root"
 
-mysql -uroot -p1234 -e "SHOW DATABASES;"
+mysql -hdb -u"$USERNAME" -p""$PASSWORD"" -e "SHOW DATABASES;"
 echo -e "Dropping a database $DATABASE... \r10% "
-mysql -uroot -p1234 -e "DROP DATABASE $DATABASE;"
+mysql -hdb -u"$USERNAME" -p"$PASSWORD" -e "DROP DATABASE $DATABASE;"
 
 echo -e "Installing DB $DATABASE... \r40% "
-mysql -uroot -p1234 -e "CREATE DATABASE IF NOT EXISTS $DATABASE CHARACTER SET utf8 COLLATE utf8_swedish_ci;"
-mysql -uroot -p1234 -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USERNAME'@localhost IDENTIFIED BY '$PASSWORD';"
-mysql -uroot -p1234 $DATABASE < /var/www/shared/db/$DATABASE.sql
+mysql -hdb -u"$USERNAME" -p"$PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DATABASE CHARACTER SET utf8 COLLATE utf8_swedish_ci;"
+mysql -hdb -u"$USERNAME" -p"$PASSWORD" -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USERNAME'@localhost IDENTIFIED BY '$PASSWORD';"
+mysql -hdb -u"$USERNAME" -p"$PASSWORD" $DATABASE < /var/www/shared/db/t3kit.sql
 
-echo -e "Restarting MySQL... \r60% "
-service mysql restart > /dev/null 2>&1
-echo -e "Restarting Apache... \r85% "
-service apache2 restart > /dev/null 2>&1
