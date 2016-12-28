185d7147b60265513d5e3d663948c6d0e8138b3e
dmh <dmytro.hrynevych@gmail.com>
2016-12-28 18:56:09 +0200

    [BUGFIX] English should be default locale (Podio bug_58)

--- restoredb.sh
+++ restoredb.sh
@@ -16,7 +16,7 @@ echo -e "Dropping a database $DATABASE... \r10% "
 mysql -h${DBHOST} -u"$USERNAME" -p"$PASSWORD" -e "DROP DATABASE $DATABASE;"
 
 echo -e "Installing DB $DATABASE... \r40% "
-mysql -h${DBHOST} -u"$USERNAME" -p"$PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DATABASE CHARACTER SET utf8 COLLATE utf8_swedish_ci;"
+mysql -h${DBHOST} -u"$USERNAME" -p"$PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -h${DBHOST} -u"$USERNAME" -p"$PASSWORD" -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$T3KIT_USER'@localhost IDENTIFIED BY '$T3KIT_PASSWORD';"
 mysql -h${DBHOST} -u"$USERNAME" -p"$PASSWORD" $DATABASE < /var/www/shared/db/t3kit.sql
 
