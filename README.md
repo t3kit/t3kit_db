bf559022e4d062398ad0fc3591ec773c3bcaaea2
dmh <dmytro.hrynevych@gmail.com>
2017-03-08 16:20:13 +0200

    [TASK] update setupdb and restoredb scripts

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ### t3kit_db [![Release](https://img.shields.io/github/release/t3kit/t3kit_db.svg?style=flat-square)](https://github.com/t3kit/t3kit_db/releases)
 
-# t3kit v7
+***
 
+## t3kit_db for t3kit v7
 
-Previous version of **t3kit_db** for **t3kit v7** you can find on branch [t3kit7](https://github.com/t3kit/t3kit_db/tree/t3kit7), or you can use git tags to chose needed version:
+Previous version of **t3kit_db** for t3kit **v7** you can find on branch [t3kit7](https://github.com/t3kit/t3kit_db/tree/t3kit7), or you can use git tags to chose needed version:
 ```
 git checkout 0.0.6
 or
--- setupdb.sh
+++ setupdb.sh
@@ -4,18 +4,12 @@ DATABASE="t3kit"
 T3KIT_USER="t3kit"
 T3KIT_PASSWORD="t3kit1234"
 
-# SHOW DATABASES
-mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e "SHOW DATABASES;"
-
-if [ "`mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e 'show databases;' | grep ${DATABASE}`" == "${DATABASE}" ]; then
-
-    # Drop database
-    echo "Dropping a database $DATABASE ..."
-    mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e "DROP DATABASE $DATABASE;"
+if [ "`mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e 'show databases;' | grep ${DATABASE}`" != "${DATABASE}" ]; then
 
     # Create new database
     echo "Installing DB $DATABASE ..."
     mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e "CREATE DATABASE IF NOT EXISTS $DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;"
     mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$T3KIT_USER'@db IDENTIFIED BY '$T3KIT_PASSWORD';"
     mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf $DATABASE < /t3kit_db/t3kit.sql
+
 fi
