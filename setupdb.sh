559c64edde06fd0e08ff08e6fab75db3edce66b2
dmh <dmytro.hrynevych@gmail.com>
2017-03-13 15:26:54 +0200

    update db scripts

--- setupdb.sh
+++ setupdb.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 DATABASE="t3kit"
-T3KIT_USER="t3kit"
-T3KIT_PASSWORD="t3kit1234"
-DBHOST="localhost"
 
 # Create new database
 echo "Creating DB $DATABASE ..."
 mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf -e "ALTER DATABASE $DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf $DATABASE < /t3kit_db/t3kit.sql
+mysql --defaults-extra-file=/t3kit_db/t3kit-mysql.cnf $DATABASE < /t3kit_db/t3kit8.sql
