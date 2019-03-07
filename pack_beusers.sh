c854fc8f2f77d42aeee6031b75ef4812ed7ec16b
dmh <dmytro.hrynevych@gmail.com>
2019-03-07 16:34:06 +0200

    [BUGFIX] fix pack_beusers.sh script

--- pack_beusers.sh
+++ pack_beusers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysqldump -uroot -p $DB_ROOT_PASSWORD -h $DB_CONTAINER_NAME "$DB_NAME" be_users > /var/www/html/vendor/t3kit/db/be_users.sql
+mysqldump -uroot -p$DB_ROOT_PASSWORD -h$DB_CONTAINER_NAME "$DB_NAME" be_users > /var/www/html/vendor/t3kit/db/be_users.sql
 echo "Done"
